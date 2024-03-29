d81cd6ce1e36d826626bc520b564c6196986bff7
Neimess <sh0ckerZ7714@gmail.com>
2024-03-29 12:35:29 +0300

    3 SONAR IN ONE

--- sonar_data.py
+++ sonar_data.py
@@ -16,6 +16,9 @@ pub_laser_center = rospy.Publisher(
     "sonar_laser_center", LaserScan, queue_size=100)
 pub_laser_right = rospy.Publisher(
     "sonar_laser_right", LaserScan, queue_size=100)
+pub_laser_merged = rospy.Publisher(
+    "merged_sonar", LaserScan, queue_size=100
+)
 pub_servo = rospy.Publisher("servo", UInt16, queue_size=10)
 
 data_left = None
@@ -39,83 +42,53 @@ def callback_center(sonars_data):
     data_center = sonars_data.data / 10
 
 
-
 scan_time = 0.1
 
 
 def laser():
     time_begin = rospy.Time.now()
-    r = rospy.Rate(10)  # 10hz
     sonar_data_left = np.zeros(60)
     sonar_data_center = np.zeros(60)
     sonar_data_right = np.zeros(60)
+    r = rospy.Rate(10)  # 10hz
+
     while not rospy.is_shutdown():
         time_end = rospy.Time.now()
 
-
-        for i in range(0, 60, 3):
+        for i in range(0, 60, 5):
             pub_servo.publish(i)
             time.sleep(scan_time)
-            laser_left = LaserScan()
-            laser_center = LaserScan()
-            laser_right = LaserScan()
-            Time=next(index)
-        
-            sonar_data_left[i] = data_left
-            
-            laser_left.header.stamp = time_end
-            laser_left.header.frame_id = 'sonar'
-            laser_left.angle_min = 0
-            laser_left.angle_max = np.pi * 90 / 180
-            laser_left.angle_increment = np.pi / 180
-            laser_left.time_increment = 0.01
-            laser_left.scan_time = scan_time
-            laser_left.range_min = 0.1
-            laser_left.range_max = 10
-            laser_left.ranges = sonar_data_left
-            laser_left.intensities = []
-            
-            sonar_data_center[i] = data_center
 
-            laser_center.header.stamp = time_end
-            laser_center.header.frame_id = 'sonar'
-            laser_center.angle_min = np.pi * 90 / 180
-            laser_center.angle_max = np.pi * 180 / 180
-            laser_center.angle_increment = np.pi / 180
-            laser_center.time_increment = 0.01
-            laser_center.scan_time = scan_time
-            laser_center.range_min = 0.1
-            laser_center.range_max = 10
-            laser_center.ranges = sonar_data_center
-            laser_center.intensities = []
-            
+            # Time = next(index)
+            sonar_data_left[i] = data_left
+            sonar_data_center[i] = data_center
             sonar_data_right[i] = data_right
 
-            laser_right.header.stamp = time_end
-            laser_right.header.frame_id = 'sonar'
-            laser_right.angle_min = np.pi * 180 / 180
-            laser_right.angle_max = np.pi * 270 / 180
-            laser_right.angle_increment = np.pi / 180
-            laser_right.time_increment = 0.01
-            laser_right.scan_time = scan_time
-            laser_right.range_min = 0.1
-            laser_right.range_max = 10
-            laser_right.ranges = sonar_data_right
-            laser_right.intensities = []
+            # Публикация объединенного LaserScan
+            laser_merged = LaserScan()
+            laser_merged.header.stamp = time_end
+            laser_merged.header.frame_id = 'sonar'
+            laser_merged.angle_min = 0
+            laser_merged.angle_max = np.pi * 120 / 180
+            laser_merged.angle_increment = np.pi / 180
+            laser_merged.time_increment = 0.01
+            laser_merged.scan_time = scan_time
+            laser_merged.range_min = 0.1
+            laser_merged.range_max = 10
+            laser_merged.ranges = np.concatenate(
+                (sonar_data_right, sonar_data_center, sonar_data_left)).tolist()
 
-            pub_laser_left.publish(laser_left)
-            pub_laser_center.publish(laser_center)
-            pub_laser_right.publish(laser_right)
+            laser_merged.intensities = []
+            pub_laser_merged.publish(laser_merged)
 
-    if i == 180:
         pub_servo.publish(0)
-    r.sleep()
+        r.sleep()
 
 
 rospy.init_node('laser', anonymous=True)
 # rospy.Subscriber('/sonar', Float64MultiArray, callback)
-rospy.Subscriber('/sonar_center', UInt16, callback_left)
-rospy.Subscriber('/sonar_left', UInt16, callback_center)
+rospy.Subscriber('/sonar_center', UInt16, callback_center)
+rospy.Subscriber('/sonar_left', UInt16, callback_left)
 rospy.Subscriber('sonar_right', UInt16, callback_right)
 
 if __name__ == '__main__':
